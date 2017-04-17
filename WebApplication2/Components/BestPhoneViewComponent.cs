3de1211c59d63735d4e5a7e13e858f527c878e51
SergerGood <serzher@inbox.ru>
2017-04-17 20:59:30 +0300

    View Component

---
+++ WebApplication2/Components/BestPhoneViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using ASP.NET.Sample.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASP.NET.Sample.Web.Components
+{
+    public class BestPhone : ViewComponent
+    {
+        List<Phone> phones;
+
+        public BestPhone()
+        {
+            phones = new List<Phone>
+            {
+                new Phone {Name="iPhone 7", Price=56000 },
+                new Phone {Name="Idol S4", Price=26000 },
+                new Phone {Name="Elite x3", Price=55000 },
+                new Phone {Name="Honor 8", Price=23000 }
+            };
+        }
+
+        public string Invoke()
+        {
+            var item = phones.OrderByDescending(x => x.Price).Take(1).FirstOrDefault();
+
+            return $"Самый дорогой телефон: {item.Name} Цена: {item.Price}";
+        }
+    }
+}
