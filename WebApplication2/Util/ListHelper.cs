9c1173b45ef7ac3131a7681d0aec5c9c44b500dd
SergerGood <serzher@inbox.ru>
2017-05-03 22:28:55 +0300

    кастомный HTML-хелпер

---
+++ WebApplication2/Util/ListHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System.Text.Encodings.Web;
+
+namespace ASP.NET.Sample.Web.Util
+{
+    public static class ListHelper
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.InnerHtml.Append(item);
+                ul.InnerHtml.AppendHtml(li);
+            }
+            ul.Attributes.Add("class", "itemsList");
+
+            var writer = new StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
