2d68f921bb091d6d6ac2d12e1271a6401b571b55
SergerGood <serzher@inbox.ru>
2017-05-03 22:47:16 +0300

    встроенные HTML-хелперы

---
+++ WebApplication2/Models/TimeOfDay.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET.Sample.Web.Models
+{
+    public enum TimeOfDay
+    {
+        [Display(Name = "Утро")]
+        Morning,
+
+        [Display(Name = "День")]
+        Afternoon,
+
+        [Display(Name = "Вечер")]
+        Evening,
+
+        [Display(Name = "Ночь")]
+        Night
+    }
+}
